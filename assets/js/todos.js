52fb4467e01d3b08440767a881152cd7e72d085f
Nicholas McCooey <nicholasmccooey@gmail.com>
2020-07-22 09:33:16 -0400

    Added Font Awesome and full functionality.

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,26 @@
-console.log("connected");
+// Check off specific todos by clicking.
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+// Click on X to delete.
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+// get input text and add to list.
+$("input[type='text']").on("keypress", function(event){
+    // check for enter key.
+    if (event.which === 13) {
+        let todoText = $(this).val();
+        $("ul").append(`<li><span><i class="fa fa-trash"></i></span> ${todoText}</li>`);
+        $(this).val("");
+    }
+})
+
+$("#plus").on("click", function(){
+    $("input[type='text']").fadeToggle();
+});
